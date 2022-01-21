9781a7725afacf398fd7badb2c2894246e0acefe
eraaly <sanazera2@gmail.com>
2022-01-21 16:58:51 +0600

    just commit

--- test_file.py
+++ test_file.py
@@ -1,2 +1,7 @@
 print('hello epta')
 print('era')
+
+n = 5
+while n != 0:
+    print('top')
+    n -= 1
---
+++ testt.py
@@ -0,0 +1,8 @@
+intVar = 21
+stringVar = 'something'
+boolVar = True
+floatVar = 3.14
+dictVar = {'name': 'Eraaly'}
+lst = [2, 4, 8, 16, 32, 64]
+
+print(intVar, stringVar, boolVar, floatVar, dictVar['name'], lst)
