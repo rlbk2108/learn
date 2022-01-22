53c0eed6bd424e76bec47e1b9a8cc12289686130
eraaly <sanazera2@gmail.com>
2022-01-22 15:57:04 +0600

    added a comment

--- testt.py
+++ testt.py
@@ -1,3 +1,4 @@
+# this is only test file
 intVar = 21
 stringVar = 'something'
 boolVar = True
@@ -6,3 +7,4 @@ dictVar = {'name': 'Eraaly'}
 lst = [2, 4, 8, 16, 32, 64]
 
 print(intVar, stringVar, boolVar, floatVar, dictVar['name'], lst)
+
